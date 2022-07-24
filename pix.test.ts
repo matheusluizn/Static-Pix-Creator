e3e2191b2f1adcf496ffaf938c4eb6236ab63aa1
mt <matheusluizn@gmail.com>
2022-07-24 19:39:14 -0300

    test: fixing unit test on class

---
+++ pix.test.ts
@@ -0,0 +1,21 @@
+import Pix from "./Pix";
+import { equal } from "assert"
+
+  ; (async () => {
+    {
+      const pixData = {
+        pixKey: "123e4567-e12b-12d1-a456-426655440000",
+        descriptionPayload: "Teste",
+        merchantName: "Fulano de Tal",
+        merchantCity: "BRASILIA",
+        txid: "123",
+        amount: 5
+      }
+      const pix = new Pix(pixData);
+      const value = pix.getPayload();
+      const testCode = "00020126670014br.gov.bcb.pix0136123e4567-e12b-12d1-a456-4266554400000205Teste52040000530398654045.005802BR5913Fulano de Tal6008BRASILIA6207050312363040E95"
+      equal(value, testCode);
+    }
+  })
+
+
